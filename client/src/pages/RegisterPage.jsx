b7525dc903f82e1f109464d719ac46c214a9fe00
youssefhammani <yhammani.dev@gmai.com>
2024-05-17 20:11:56 +0100

    chore: Add axios dependency and implement user registration functionality

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,14 +1,47 @@
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { useState } from "react";
 
 export default function RegisterPage() {
+    const [username, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (ev) => {
+        ev.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:3000/api/auth/register', {
+                username,
+                email,
+                password
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Register</h1>
-                <form className="max-w-md mx-auto">
-                    <input type="text" placeholder="Your name" />
-                    <input type="email" placeholder="your@email.com" />
-                    <input type="password" placeholder="password" />
+                <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+                    <input type="text"
+                        placeholder="Your name"
+                        value={username}
+                        onChange={(ev) => setUserName(ev.target.value)}
+                    />
+                    <input type="email"
+                        placeholder="your@email.com"
+                        value={email}
+                        onChange={(ev) => setEmail(ev.target.value)}
+                    />
+                    <input type="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={(ev) => setPassword(ev.target.value)}
+                    />
                     <button className="primary">Login</button>
                     <div className="text-center py-2 text-gray-500">
                         Already a member? <Link className="underline text-black" to={'/login'}>Login</Link>
